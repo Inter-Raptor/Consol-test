1ca540bdfe0d8f12f99a6460e166a63bd3d10dd7
Inter-Raptor <VivienJardotID@outlook.com>
2026-04-28 23:26:33 +0200

    ok

--- retro-go-master/components/retro-go/targets/consol-test/config.h
+++ retro-go-master/components/retro-go/targets/consol-test/config.h
@@ -26,25 +26,16 @@
 #define RG_SCREEN_ROTATE            0
 #define RG_SCREEN_VISIBLE_AREA      {0, 0, 0, 0}
 #define RG_SCREEN_SAFE_AREA         {0, 0, 0, 0}
-#define RG_SCREEN_INIT()                                                                                         \
-    ILI9341_CMD(0xCF, 0x00, 0xc3, 0x30);                                                                         \
-    ILI9341_CMD(0xED, 0x64, 0x03, 0x12, 0x81);                                                                   \
-    ILI9341_CMD(0xE8, 0x85, 0x00, 0x78);                                                                         \
-    ILI9341_CMD(0xCB, 0x39, 0x2c, 0x00, 0x34, 0x02);                                                             \
-    ILI9341_CMD(0xF7, 0x20);                                                                                     \
-    ILI9341_CMD(0xEA, 0x00, 0x00);                                                                               \
-    ILI9341_CMD(0xC0, 0x1B);                 /* Power control   //VRH[5:0] */                                    \
-    ILI9341_CMD(0xC1, 0x12);                 /* Power control   //SAP[2:0];BT[3:0] */                            \
-    ILI9341_CMD(0xC5, 0x32, 0x3C);           /* VCM control */                                                   \
-    ILI9341_CMD(0xC7, 0x91);                 /* VCM control2 */                                                  \
-    ILI9341_CMD(0x36, 0x08);                 /* Memory Access Control  (BGR) */                                  \
-    ILI9341_CMD(0xB1, 0x00, 0x10);           /* Frame Rate Control */                                            \
-    ILI9341_CMD(0xB6, 0x0A, 0xA2);           /* Display Function Control */                                      \
-    ILI9341_CMD(0xF6, 0x01, 0x30);                                                                               \
-    ILI9341_CMD(0xF2, 0x00);                 /* 3Gamma Function Disable */                                       \
-    ILI9341_CMD(0x21);                       /* Display inversion on (matches Arduino test sketch) */            \
-    ILI9341_CMD(0xE0, 0xD0, 0x00, 0x05, 0x0E, 0x15, 0x0D, 0x37, 0x43, 0x47, 0x09, 0x15, 0x12, 0x16, 0x19);       \
-    ILI9341_CMD(0xE1, 0xD0, 0x00, 0x05, 0x0D, 0x0C, 0x06, 0x2D, 0x44, 0x40, 0x0E, 0x1C, 0x18, 0x16, 0x19);
+#define RG_SCREEN_INIT() \
+    ILI9341_CMD(0x01); \
+    ILI9341_DELAY(150); \
+    ILI9341_CMD(0x11); \
+    ILI9341_DELAY(150); \
+    ILI9341_CMD(0x3A, 0x55); \
+    ILI9341_CMD(0x36, 0x00); \
+    ILI9341_CMD(0x21); \
+    ILI9341_CMD(0x29); \
+    ILI9341_DELAY(50);
 
 // Input
 #define RG_GAMEPAD_GPIO_MAP {\
@@ -75,7 +66,7 @@
 #define RG_GPIO_LCD_CS              GPIO_NUM_NC
 #define RG_GPIO_LCD_DC              GPIO_NUM_38
 #define RG_GPIO_LCD_BCKL            GPIO_NUM_42
-// #define RG_GPIO_LCD_BCKL_INVERT
+#define RG_GPIO_LCD_BCKL_INVERT
 #define RG_GPIO_LCD_RST             GPIO_NUM_41
 
 // SD Card (SDMMC 1-bit mode)
