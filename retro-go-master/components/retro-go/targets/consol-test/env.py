083fd46aab4a36c3aa212c5b7ef09f75e2ee26d1
Inter-Raptor <VivienJardotID@outlook.com>
2026-05-01 09:45:57 +0200

    ok

--- retro-go-master/components/retro-go/targets/consol-test/env.py
+++
@@ -1,7 +0,0 @@
-# This file is injected late into rg_tool.py, you can run arbitrary python code here
-# For example override python variables or set environment variables with os.putenv
-
-# Espressif chip in the device
-IDF_TARGET = "esp32s3"
-# .fw file format, if supported by the device
-FW_FORMAT = "none"
